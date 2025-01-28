dd097abcf3efc2f6493ed25f18a750386e63f1ef
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-01-28 13:11:02 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+name = input("Name: ")
+
+print("Hello", name)
+print("give me a number: ")
+test = input()
+age = input("Age: ")
+print("You are", age, "years old")
+
+age_num = int(age)
+next_year = age_num + 1
+print("Next year you will be", next_year)
+
+price = float(input("Price: "))
+
+after_tax = price * 1.1
+
+print("After taxes:", after_tax)
